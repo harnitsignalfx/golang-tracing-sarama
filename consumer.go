5fb6a4ec95e79ec15c4ebf548d75921ec538c607
Harnit Singh <harnit.singh@signalfx.com>
2020-10-28 16:25:19 -0700

    working example for kafka

---
+++ consumer.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+	"fmt"
+
+	saramatrace "github.com/signalfx/signalfx-go-tracing/contrib/Shopify/sarama"
+	"github.com/signalfx/signalfx-go-tracing/ddtrace/tracer"
+	"github.com/signalfx/signalfx-go-tracing/tracing"
+	sarama "gopkg.in/Shopify/sarama.v1"
+)
+
+
+func Example_consumer() {
+	tracing.Start(tracing.WithServiceName("Kafka-Consumer"),tracing.WithEndpointURL("https://ingest.<realm>.signalfx.com/v2/trace"),
+		tracing.WithAccessToken("<insert-token>"),tracing.WithGlobalTag("environment","Kafka-Testing"))
+	
+	defer tracing.Stop()
+	consumer, err := sarama.NewConsumer([]string{"<broker-ip>:9092"}, nil)
+	if err != nil {
+		panic(err)
+	}
+	defer consumer.Close()
+
+	consumer = saramatrace.WrapConsumer(consumer)
+
+	partitionConsumer, err := consumer.ConsumePartition("TestTopic", 0, sarama.OffsetNewest)
+	if err != nil {
+		panic(err)
+	}
+	defer partitionConsumer.Close()
+
+	consumed := 0
+	for msg := range partitionConsumer.Messages() {
+		// if you want to use the kafka message as a parent span:
+		if spanctx, err := tracer.Extract(saramatrace.NewConsumerMessageCarrier(msg)); err == nil {
+			// you can create a span using ChildOf(spanctx)
+			_ = spanctx
+		}
+
+		log.Printf("Consumed message offset %d\n", msg.Offset)
+		consumed++
+	}
+}
+
+func main() {
+	
+
+	fmt.Println("Calling consumer")
+	Example_consumer()
+	fmt.Println("done..")
+}
---
+++ producer.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"math/rand"
+
+	saramatrace "github.com/signalfx/signalfx-go-tracing/contrib/Shopify/sarama"
+	"github.com/signalfx/signalfx-go-tracing/tracing"
+	sarama "gopkg.in/Shopify/sarama.v1"
+	opentracing "github.com/opentracing/opentracing-go"
+)
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+func RandStringBytesMask(n int) string {
+	b := make([]byte, n)
+	for i := 0; i < n; {
+		if idx := int(rand.Int63() & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i++
+		}
+	}
+	return string(b)
+}
+
+func Example_syncProducer() {
+	fmt.Println("global tracer->",opentracing.GlobalTracer())
+	tracing.Start(tracing.WithServiceName("Kafka-Producer"),tracing.WithEndpointURL("https://ingest.<realm>.signalfx.com/v2/trace"),
+		tracing.WithAccessToken("<my-token>"),tracing.WithGlobalTag("environment","Kafka-Testing"))
+	
+	defer tracing.Stop()
+
+	cfg := sarama.NewConfig()
+	cfg.Producer.Return.Successes = true
+
+	producer, err := sarama.NewSyncProducer([]string{"<broker-ip>:9092"}, cfg)
+	if err != nil {
+		panic(err)
+	}
+	defer producer.Close()
+
+	producer = saramatrace.WrapSyncProducer(cfg, producer)
+
+	msg := &sarama.ProducerMessage{
+		Topic: "TestTopic",
+		Value: sarama.StringEncoder(RandStringBytesMask(8)),
+	}
+	_, _, err = producer.SendMessage(msg)
+	if err != nil {
+		panic(err)
+	}
+
+	
+	fmt.Println("global tracer->",opentracing.GlobalTracer())
+
+}
+
+func main() {
+	
+	for {
+		fmt.Println("Calling syncproducer")
+		Example_syncProducer()
+		fmt.Println("done..")
+		time.Sleep(2*time.Second)
+	}
+	
+}
